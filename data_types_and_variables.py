154d777f70d0158458a42a4f9dbe2076bdccc372
ellaxu43 <ella.xu43@gmail.com>
2022-01-19 15:41:26 -0600

    add more files

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -25,23 +25,54 @@ True or "42"
 {} + {}
 #Exercises Part 2
 #movies for your kids:
-movie_cost = 5 *3 + 1*3 +3*3 
-movie_cost
+lit_mer = 3
+bro_bear = 5
+hercules = 1
+
+rentallist = [lit_mer, bro_bear, hercules]
+price = sum(rentallist) * 3 
+
+print(price)
 
 #receive payment
-payment = 400*6 + 350*10 + 380* 4
-payment
+amazon = 380
+google = 400
+facebook = 350
+
+amazon_work_hours = 4
+google_work_hours = 6
+facebook_work_hours = 10
+
+paycheck = amazon* amazon_work_hours + google*google_work_hours + facebook* facebook_work_hours
+print(paycheck)
+print(f'total amount is {paycheck}')
 
 # student question
 
+class_not_full = True
+
+no_conflict = True 
+
+enrolled = class_not_full and no_conflict 
+
+print(f'Can student enroll? \n {enrolled}')
+
 # product offer question 
+premium_member = True
+offer_not_expired = True 
+qualifying_items = 3 
+discount_applied = offer_not_expired and (qualifying_items > 2 or premium_member)
+
 
 #username and password question 
-username = 'codeup'
-password = 'notastrongpassword'
-username != password
-len(username) <= 20 
-len(password) >= 5
+username= 'codeup'
+password = 'password'
+
+if len(username) <= 20 and len(password) >= 5 and username != password:
+    print('username and password registered')
+else: 
+    'the password must be at least 5 characters, the username must be no more than 20 characters, the password must not be the same as the username'
+
 
 
 
